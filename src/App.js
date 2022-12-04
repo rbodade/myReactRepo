3655ee184b57ec3927d9f19deb09cb46cdf9a048
rbodade <rhushikesh.bodade@gmail.com>
2022-12-04 12:59:19 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          An Amazing Startup idea from Rhushikesh - PROD!
         </p>
         <a
           className="App-link"
